5043b32bb48e6a51718f173ce4c61dab5e6f3d29
EddyBautista <bautista.j.eddy@gmail.com>
2021-05-01 09:28:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace May2021_Coding_Questions
+{
+    class Program
+    {
+        //5/01/2021
+
+        //Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
+        //without any elements with the same value next to each other and preserving the original order of elements.
+
+        //For example:
+
+        //uniqueInOrder("AAAABBBCCDAABBB") == {'A', 'B', 'C', 'D', 'A', 'B'}
+        //uniqueInOrder("ABBCcAD")         == {'A', 'B', 'C', 'c', 'A', 'D'}
+        //uniqueInOrder([1,2,2,3,3])       == {1,2,3}
+
+
+          public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            //your code here...
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
